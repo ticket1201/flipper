e0cc8370516200951b158622f85bdc31bfaa33ea
Anthony <79248913+ticket1201@users.noreply.github.com>
2021-10-26 22:59:42 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-//here is code
+
+var videoOne = document.getElementById('bgVideo');
+var imageOne = document.getElementById('billyImg');
+play = function(){
+    videoOne.play();
+}
+imageOne.addEventListener("click", play);
